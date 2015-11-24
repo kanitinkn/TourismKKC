3308e3e4b49fd4b21688de5121bc491d8ff0dff7
Near River <jimmy29922@gmail.com>
2015-11-24 16:50:22 +0700

    Change API

--- app/src/main/java/com/tourismkkc/APIconnect.java
+++ app/src/main/java/com/tourismkkc/APIconnect.java
@@ -1,6 +1,5 @@
 package com.tourismkkc;
 
-import android.os.AsyncTask;
 import android.util.Log;
 
 import com.squareup.okhttp.FormEncodingBuilder;
@@ -14,8 +13,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
-public class APIConnect extends AsyncTask<Void, Void, String> {
-    private String URL = "http://192.168.0.132/TourismKKC/main/register";
+public class APIConnect {
     private String URL_MAIN = "http://192.168.0.132/TourismKKC/main/";
     private String TAG = "CBNUKE_Dev";
     private OkHttpClient okHttpClient = new OkHttpClient();
@@ -105,97 +103,97 @@ public class APIConnect extends AsyncTask<Void, Void, String> {
         return apiStatus;
     }
 
-    @Override
-    protected String doInBackground(Void... voids) {
-        OkHttpClient okHttpClient = new OkHttpClient();
-
-        RequestBody formBody = new FormEncodingBuilder()
-                .add("user_email", "test@test.com")
-                .add("user_password", "1234")
-                .add("user_fname", "Test")
-                .add("user_lname", "Com")
-                .build();
-
-        /*RequestBody requestBody = new MultipartBuilder()
-                .type(MultipartBuilder.FORM)
-                .addPart(
-                        Headers.of("Content-Disposition", "form-data; name=\"user_email\""),
-                        RequestBody.create(null, "test@test.com"))
-                .addPart(
-                        Headers.of("Content-Disposition", "form-data; name=\"user_password\""),
-                        RequestBody.create(null, "1234"))
-                .addPart(
-                        Headers.of("Content-Disposition", "form-data; name=\"user_fname\""),
-                        RequestBody.create(null, "Test"))
-                .addPart(
-                        Headers.of("Content-Disposition", "form-data; name=\"user_lname\""),
-                        RequestBody.create(null, "Com"))
-                .addPart(
-                        Headers.of("Content-Disposition", "form-data; name=\"image\""),
-                        RequestBody.create(MEDIA_TYPE_PNG, new File("website/static/logo-square.png")))
-                .build();*/
-
-        Request.Builder builder = new Request.Builder();
-        String URL = "http://192.168.0.132/TourismKKC/main/register";
-        Request request = builder.url(URL).post(formBody).build();
-
-        Log.d(TAG, "Start API");
-
-        try {
-            Response response = okHttpClient.newCall(request).execute();
-
-            if (response.isSuccessful()) {
-                return response.body().string();
-            } else {
-                Log.d(TAG, "Not Success - code : " + response.code());
-                return "Not Success - code : " + response.code();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.d(TAG, "Error - " + e.getMessage());
-            return "Error - " + e.getMessage();
-        }
-    }
-
-    @Override
-    protected void onPostExecute(String string) {
-
-        super.onPostExecute(string);
-        Log.d(TAG, "End API");
-        Log.d(TAG, string);
-
-        try {
-            JSONObject jStatus = new JSONObject(string);
-            String status = jStatus.getString("status");
-            Log.d(TAG, "STATUS:" + status);
-
-            JSONObject objData = jStatus.getJSONObject("data");
-            String action = objData.getString("action");
-            Log.d(TAG, "ACTION:" + action);
-
-            String reason = objData.getString("reason");
-            Log.d(TAG, "REASON:" + reason);
-
-/*            JSONArray jArray = jObject.getJSONArray("data");
-            for (int i = 0; i < jArray.length(); i++) {
-                try {
-                    Log.d(TAG, "Loop " + i);
-                    JSONObject oneObject = jArray.getJSONObject(i);
-                    // Pulling items from the array
-                    String oneObjectsItem = oneObject.getString("action");
-                    String oneObjectsItem2 = oneObject.getString("reason");
-                    Log.d(TAG, oneObjectsItem);
-                    Log.d(TAG, oneObjectsItem2);
-                } catch (JSONException e) {
-                    // Oops
-                }
-            }*/
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-            Log.d(TAG, "ERROR " + e.toString());
-        }
-
-    }
+//    @Override
+//      protected String doInBackground(Void... voids) {
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//
+//        RequestBody formBody = new FormEncodingBuilder()
+//                .add("user_email", "test@test.com")
+//                .add("user_password", "1234")
+//                .add("user_fname", "Test")
+//                .add("user_lname", "Com")
+//                .build();
+//
+//        /*RequestBody requestBody = new MultipartBuilder()
+//                .type(MultipartBuilder.FORM)
+//                .addPart(
+//                        Headers.of("Content-Disposition", "form-data; name=\"user_email\""),
+//                        RequestBody.create(null, "test@test.com"))
+//                .addPart(
+//                        Headers.of("Content-Disposition", "form-data; name=\"user_password\""),
+//                        RequestBody.create(null, "1234"))
+//                .addPart(
+//                        Headers.of("Content-Disposition", "form-data; name=\"user_fname\""),
+//                        RequestBody.create(null, "Test"))
+//                .addPart(
+//                        Headers.of("Content-Disposition", "form-data; name=\"user_lname\""),
+//                        RequestBody.create(null, "Com"))
+//                .addPart(
+//                        Headers.of("Content-Disposition", "form-data; name=\"image\""),
+//                        RequestBody.create(MEDIA_TYPE_PNG, new File("website/static/logo-square.png")))
+//                .build();*/
+//
+//        Request.Builder builder = new Request.Builder();
+//        String URL = "http://192.168.0.132/TourismKKC/main/register";
+//        Request request = builder.url(URL).post(formBody).build();
+//
+//        Log.d(TAG, "Start API");
+//
+//        try {
+//            Response response = okHttpClient.newCall(request).execute();
+//
+//            if (response.isSuccessful()) {
+//                return response.body().string();
+//            } else {
+//                Log.d(TAG, "Not Success - code : " + response.code());
+//                return "Not Success - code : " + response.code();
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            Log.d(TAG, "Error - " + e.getMessage());
+//            return "Error - " + e.getMessage();
+//        }
+//    }
+//
+//    @Override
+//    protected void onPostExecute(String string) {
+//
+//        super.onPostExecute(string);
+//        Log.d(TAG, "End API");
+//        Log.d(TAG, string);
+//
+//        try {
+//            JSONObject jStatus = new JSONObject(string);
+//            String status = jStatus.getString("status");
+//            Log.d(TAG, "STATUS:" + status);
+//
+//            JSONObject objData = jStatus.getJSONObject("data");
+//            String action = objData.getString("action");
+//            Log.d(TAG, "ACTION:" + action);
+//
+//            String reason = objData.getString("reason");
+//            Log.d(TAG, "REASON:" + reason);
+//
+///*            JSONArray jArray = jObject.getJSONArray("data");
+//            for (int i = 0; i < jArray.length(); i++) {
+//                try {
+//                    Log.d(TAG, "Loop " + i);
+//                    JSONObject oneObject = jArray.getJSONObject(i);
+//                    // Pulling items from the array
+//                    String oneObjectsItem = oneObject.getString("action");
+//                    String oneObjectsItem2 = oneObject.getString("reason");
+//                    Log.d(TAG, oneObjectsItem);
+//                    Log.d(TAG, oneObjectsItem2);
+//                } catch (JSONException e) {
+//                    // Oops
+//                }
+//            }*/
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//            Log.d(TAG, "ERROR " + e.toString());
+//        }
+//
+//    }
 }
 
